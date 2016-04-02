37df0a305224344facb5e2d523ca1408aba67c50
Mizunotai <mizutai731@gmail.com>
2016-04-02 10:18:17 +0900

    completion

--- app/src/main/java/com/example/taiki/memoapplicaton/Main2Activity.kt
+++ app/src/main/java/com/example/taiki/memoapplicaton/Main2Activity.kt
@@ -1,9 +1,8 @@
 package com.example.taiki.memoapplicaton
 
 import android.content.SharedPreferences
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.support.v7.app.AppCompatActivity
 import android.widget.Button
 import android.widget.EditText
 
@@ -16,13 +15,13 @@ class Main2Activity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
         var editText :EditText = findViewById(R.id.editText) as EditText
-        var button :Button = findViewById(R.id.button) as Button
+        var button2 :Button = findViewById(R.id.button2) as Button
 
         pref = getSharedPreferences("KEY", MODE_PRIVATE)
         edit = pref?.edit()
 
 
-        button.setOnClickListener {
+        button2.setOnClickListener {
             var memoText: String = editText.text.toString()
 
             edit?.putString("SAVE",memoText)
@@ -30,8 +29,5 @@ class Main2Activity : AppCompatActivity() {
 
             finish()
         }
-
-
-
     }
 }
--- app/src/main/java/com/example/taiki/memoapplicaton/MainActivity.kt
+++ app/src/main/java/com/example/taiki/memoapplicaton/MainActivity.kt
@@ -2,34 +2,56 @@ package com.example.taiki.memoapplicaton
 
 import android.content.Intent
 import android.content.SharedPreferences
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    var pref: SharedPreferences? = null
-//    var edit: SharedPreferences.Editor? = null
-
+    var pref1: SharedPreferences? = null
+    //    var edit: SharedPreferences.Editor? = null
+    var textView: TextView? = null
+    var button :Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var textView: TextView = findViewById(R.id.textView) as TextView
+        textView = findViewById(R.id.textView) as TextView
 
-        var button: Button = findViewById(R.id.textView) as Button
+        button = findViewById(R.id.button) as Button
 
-        textView.text = pref?.getString("SAVE",toString())
+        pref1 = getSharedPreferences("KEY", MODE_PRIVATE)
 
-        button.setOnClickListener {
+        var texts: String = pref1?.getString("SAVE", "").toString()
+
+        Log.e("main_1", texts)
+
+        textView?.text = texts
+
+
+
+
+        button?.setOnClickListener {
 
 
             var intent = Intent(applicationContext, Main2Activity::class.java)
             startActivity(intent)
 
         }
+    }
 
+    override fun onResume() {
+        super.onResume()
+
+        var texts: String = pref1?.getString("SAVE", "").toString()
+
+        Log.e("main_1", texts)
+
+        textView?.text = texts
 
     }
+
+
 }
